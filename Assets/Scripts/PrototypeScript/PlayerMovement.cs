aa8ad2030503386a2bbc626bca5dacf0f0af39b1
Mostafa Haque <93haque@gmail.com>
2019-01-06 17:50:22 -0500

    Basic movement

---
+++ Assets/Scripts/PrototypeScript/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public float moveSpeed;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Move();
+	}
+
+	void Move()
+	{
+		if (Input.GetAxisRaw("Horizontal") != 0)
+		{
+			this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+			
+			this.GetComponent<Rigidbody2D>().velocity =
+				new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * moveSpeed, 0.8f), 0);
+
+			if (Input.GetAxisRaw("Horizontal") > 0)
+			{
+				this.transform.Find("MimSprite").GetComponent<SpriteRenderer>().flipX = false;
+			}else if (Input.GetAxisRaw("Horizontal") < 0)
+			{
+				this.transform.Find("MimSprite").GetComponent<SpriteRenderer>().flipX = true;
+			}
+			
+		}
+
+		else
+		{
+			this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY|RigidbodyConstraints2D.FreezePositionX|RigidbodyConstraints2D.FreezeRotation;
+		}
+	}
+}
