813d30083a9594ba1d5a4e4f4d0f5ad72d2ecc14
Mostafa Haque <93haque@gmail.com>
2019-01-06 18:04:40 -0500

    Layers working!

--- Assets/Scripts/PrototypeScript/PlayerStats.cs
+++ Assets/Scripts/PrototypeScript/PlayerStats.cs
@@ -5,15 +5,56 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
 
-	public float fear;
+	
+	public Animator playerAnim;
+
+	public int fear;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		playerAnim = GameObject.FindWithTag("Player").GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		FearChange();
+		IncreaseFear();
+	}
+
+
+	public void FearChange()
+	{
+		if (fear == 0)
+		{
+			playerAnim.SetLayerWeight(0,1);
+			playerAnim.SetLayerWeight(1,0);
+			playerAnim.SetLayerWeight(2,0);
+		}
+		else if (fear == 1)
+		{
+			playerAnim.SetLayerWeight(0,0);
+			playerAnim.SetLayerWeight(1,1);
+			playerAnim.SetLayerWeight(2,0);
+		}
+		else if (fear == 2)
+		{
+			playerAnim.SetLayerWeight(0,0);
+			playerAnim.SetLayerWeight(1,0);
+			playerAnim.SetLayerWeight(2,1);
+		}
+		else
+		{
+			fear = 0;
+		}
+	}
+
+	public void IncreaseFear()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			fear++;
+		}
 	}
 }
