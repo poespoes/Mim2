6636c5815dd52ed2404fd3154fd13de49f9c8cba
Mostafa Haque <93haque@gmail.com>
2019-02-02 14:44:16 -0500

    Mim walk animation linked

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -24,11 +24,14 @@ public class PlayerMovement : MonoBehaviour
 	public float groundRadius;
 	public Transform groundPoint;
 
+	public Animator anim;
+
 	
 	// Use this for initialization
 	void Start ()
 	{
 
+		anim = this.GetComponent<Animator>();
 		rb = this.GetComponent<Rigidbody2D>();
 	}
 	
@@ -61,6 +64,7 @@ public class PlayerMovement : MonoBehaviour
 		{
 			//rb.constraints = RigidbodyConstraints2D.None;
 			
+			anim.SetBool("isWalking",true);
 			
 			rb.velocity =
 				new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * moveSpeed, 0.8f), rb.velocity.y);
@@ -76,6 +80,8 @@ public class PlayerMovement : MonoBehaviour
 				this.transform.Find("MimSprite").GetComponent<SpriteRenderer>().flipX = true;
 			}
 			
+			
+			
 		}
 
 		else
@@ -84,6 +90,8 @@ public class PlayerMovement : MonoBehaviour
 
 			float horizontalSpeed=0;
 			DOTween.To(()=> horizontalSpeed, x=> horizontalSpeed = x, 0, 1);
+			
+			anim.SetBool("isWalking",false);
 			
 			rb.velocity = new Vector2(horizontalSpeed,rb.velocity.y);
 
