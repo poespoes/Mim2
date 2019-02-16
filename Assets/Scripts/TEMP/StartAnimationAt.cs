b615f2c9301e53fa21a5d57204f9d9676b6b7fe4
nobo <lephira@gmail.com>
2019-02-15 20:25:47 -0500

    Added anim delay

---
+++ Assets/Scripts/TEMP/StartAnimationAt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimationAt : MonoBehaviour
+{
+    private Animator anim;
+    private bool gameStarted;
+
+    [Range(0, 1)]
+    public float StartingAt;
+    public string AnimationName;
+
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (gameStarted == false) {
+            anim.Play(AnimationName, 0, StartingAt);
+            gameStarted = true;
+        }
+    }
+}
