1732e2eed97cd2f84242e57a37496883375b6ffc
Mostafa Haque <93haque@gmail.com>
2019-02-24 14:19:26 -0500

    IK chases player

---
+++ Assets/MoveTowardsPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+
+    public Transform target;
+    public bool isTriggered;
+    public VineSine vineSine;
+
+
+    private void Awake()
+    {
+        //target = PlayerStats.playerTransform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTriggered == true)
+        {
+            Chase();
+            vineSine.enabled = false;
+        }
+        else
+        {
+            vineSine.enabled = true;
+        }
+    }
+
+    public void Chase()
+    {
+        //this.transform.DOMove(target.position, 2);
+
+        this.transform.DOMoveX(target.position.x, 2);
+        this.transform.DOMoveY(target.position.y, 2);
+    }
+}
