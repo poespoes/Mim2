99083b8c27120fad98dd74161c7e03335e10fa3d
Mostafa Haque <93haque@gmail.com>
2019-03-03 15:42:05 -0500

    Leaf animations synced up

---
+++ Assets/Scripts/MimLeaf.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimLeaf : MonoBehaviour
+{
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (PlayerStats.mimLit == true)
+        {
+            anim.SetBool("LeafON",false);
+            anim.SetBool("LeafOFF",true);
+        }
+        else
+        {
+            anim.SetBool("LeafON",true);
+            anim.SetBool("LeafOFF",false);
+                
+        }
+    }
+}
