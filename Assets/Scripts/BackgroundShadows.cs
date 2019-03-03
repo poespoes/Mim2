18536a07b290882d15420e6edc46c9b10b2ef384
Mostafa Haque <93haque@gmail.com>
2019-03-03 14:40:31 -0500

    Mim light tweak

---
+++ Assets/Scripts/BackgroundShadows.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class BackgroundShadows : MonoBehaviour
+{
+
+    public SpriteRenderer _frame1;
+    public SpriteRenderer _frame3;
+    public float alpha;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = _frame1.color.a; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerStats.mimLit == true)
+        {
+            _frame1.DOFade(alpha, 0.25f);
+            _frame3.DOFade(0, 0.5f);
+        }
+        else
+        {
+            
+            _frame1.DOFade(0, 0.5f);
+            _frame3.DOFade(alpha, 0.25f);
+        }
+    }
+}
