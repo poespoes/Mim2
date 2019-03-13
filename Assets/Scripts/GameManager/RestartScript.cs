a5a1dfcce7cde75097f266bd532be658a09d9b5d
Unknown <poe_s@hotmail.com>
2019-03-13 15:16:46 -0400

    Menu, end and restart buttons

---
+++ Assets/Scripts/GameManager/RestartScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour
+{
+    public KeyCode BackToMenu;
+    public KeyCode RestartToCheckPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(RestartToCheckPoint)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyUp(BackToMenu)) {
+            SceneManager.LoadScene("Menu");
+        }
+
+    }
+}
