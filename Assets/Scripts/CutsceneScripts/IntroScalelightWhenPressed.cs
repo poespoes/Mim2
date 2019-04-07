c43411dd7a7360498e775180092db7b3901f6546
Mostafa Haque <93haque@gmail.com>
2019-04-07 18:01:54 -0400

    Animation scripts in intro tutorial

---
+++ Assets/Scripts/CutsceneScripts/IntroScalelightWhenPressed.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScalelightWhenPressed : MonoBehaviour
+{
+    public Transform leaf;
+    public bool maxSizeReached;
+
+    public float lightDecreaseAmount;
+    private float lightDecreaseActual;
+
+
+    public Animator vineAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lightDecreaseAmount == 0)
+        {
+            lightDecreaseAmount = 1;
+        }
+        lightDecreaseActual = lightDecreaseAmount / 100;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1") && leaf.transform.localScale.x < 1)
+        {
+            leaf.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+           
+        }
+
+
+        if (leaf.transform.localScale.x >= 1)
+        {
+
+            maxSizeReached = true;
+            LightDeath();
+
+        }
+
+
+        if (maxSizeReached == false && leaf.transform.localScale.x > 0)
+        {
+            leaf.transform.localScale -= new Vector3(lightDecreaseActual,lightDecreaseActual,lightDecreaseActual);
+        }
+
+
+    }
+
+
+    void LightDeath()
+    {
+        vineAnim.SetTrigger("playAnimation");
+    }
+}
