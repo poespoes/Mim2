228be9a1157e53d33d01137f2ca8a2299e80aae6
Mostafa Haque <93haque@gmail.com>
2019-04-07 18:19:59 -0400

    More cutscene scenes

---
+++ Assets/Scripts/CutsceneScripts/CutsceneTransition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTransition : MonoBehaviour
+{
+
+    public List<GameObject> objectsToEnable;
+    public List<GameObject> objectsToDisable;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Next()
+    {
+        foreach (GameObject objectToDisable in objectsToDisable)
+        {
+            objectToDisable.SetActive(false);
+        }
+        
+        foreach (GameObject objectToEnable in objectsToEnable)
+        {
+            objectToEnable.SetActive(true);
+        }
+    }
+
+    public void FadeSprites(GameObject _gameObject)
+    {
+        
+      
+        
+        
+    }
+    
+    
+}
---
+++ Assets/Scripts/CutsceneScripts/OnTriggerEventStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEventStart : MonoBehaviour
+{
+
+    public UnityEvent unityEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            unityEvent.Invoke();
+        }
+    }
+}
