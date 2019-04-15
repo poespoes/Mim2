26446f49e17214899b0bd52a051e174b71cdbc5e
Mostafa Haque <93haque@gmail.com>
2019-04-14 23:38:05 -0400

    Script restaged

---
+++ Assets/Scripts/GenericEventActivator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericEventActivator : MonoBehaviour
+{
+    public UnityEvent unityEvents;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartEvents()
+    {
+        unityEvents.Invoke();
+    }
+    
+    
+}
---
+++ Assets/Scripts/TriggerExitEventActivator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerExitEventActivator : MonoBehaviour
+{
+    public UnityEvent triggerExitUnityEvent;
+
+    public UnityEvent triggerEnterUnityEvent;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartExitEvents();
+        }
+    }
+    
+    public void StartExitEvents()
+    {
+        if (triggerExitUnityEvent != null)
+        {
+            triggerExitUnityEvent.Invoke();
+        }
+        
+    }
+
+    public void StartEnterEvents()
+    {
+        if (triggerEnterUnityEvent != null)
+        {
+            triggerEnterUnityEvent.Invoke();
+        }
+    }
+}
